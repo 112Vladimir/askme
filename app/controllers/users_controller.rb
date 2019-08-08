19ad992abc64a376f3609d35a50aa1b0bd2f52f5
112Vladimir <new-work@list.ru>
2019-08-08 22:31:02 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Hello Man"
+    @user = User.new(
+        id: 1,
+        name: 'Vladimir',
+        username: '112Vladimir',
+        avatar_url:'https://okotikah.ru/wp-content/uploads/2019/01/samya-umnaya-koshka.png'
+    )
 
+    @questions = [
+        Question.new(text: 'How are you?', created_at: Date.parse('07.08.2019')),
+    Question.new(text: 'What are you doing?', created_at: Date.parse('07.08.2019')),
+    ]
+
+    @new_question = Question.new
   end
 end
